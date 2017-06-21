2aabbde06ab1137ddc563f69d98cec72aefdf164
Chad Martin <chad.martin@ametritech.com>
2017-06-20 20:38:45 -0700

    Hash test 100 rounds 10 tails

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 
+
 var cf = require('./coin-flip-service.js');
 var testRounds = 100;
 var resultList = [];
 
 do
 {
-var result = cf.main(5);
+var result = cf.main(10);
 resultList.push(result);
 
 }while(resultList.length != testRounds)
